47ad7003811a78cbda00177918e9954e57ebab80
Sevoyan <sevoyanmxitar7@gmail.com>
2026-07-31 21:04:47 +0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,13 @@
+document.getElementById("login-form").addEventListener("submit", function(e){
 
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+
+    localStorage.setItem("user", username);
+
+    alert("Բարի գալուստ, " + username + "!");
+
+    window.location.href = "index.html";
+
+});
