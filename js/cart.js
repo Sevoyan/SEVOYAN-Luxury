f3d5e44849727590da43105ed3d70ded9721e016
Sevoyan <sevoyanmxitar7@gmail.com>
2026-08-05 21:15:45 +0400

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -5,45 +5,38 @@ function loadCart() {
     if (!container) return;
 
     if (cart.length === 0) {
-        container.innerHTML = "<h2>🛒 Զամբյուղը դատարկ է</h2>";
+        container.innerHTML = "<h2>Զամբյուղը դատարկ է</h2>";
         return;
     }
 
-    let total = 0;
     let html = "";
+    let total = 0;
 
     cart.forEach((item, index) => {
-        total += item.price * (item.qty || 1);
+        total += Number(item.price);
 
         html += `
         <div class="product-card">
             <h3>${item.name}</h3>
             <p>$${item.price}</p>
-            <p>Քանակ՝ ${item.qty || 1}</p>
-
             <button onclick="removeItem(${index})">
-                ❌ Հեռացնել
+                ❌ Ջնջել
             </button>
-        </div>
-        `;
+        </div>`;
     });
 
     html += `
-        <h2 style="margin-top:30px;">
-            Ընդամենը՝ $${total}
-        </h2>
-    `;
+        <h2 style="margin-top:30px">
+            Ընդհանուր՝ $${total}
+        </h2>`;
 
     container.innerHTML = html;
 }
 
 function removeItem(index) {
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
     cart.splice(index, 1);
-
     localStorage.setItem("cart", JSON.stringify(cart));
-
     loadCart();
 }
 
