3c62f1dec3f9d95c6504f9e65d4eb7f5a089c23e
Sevoyan <sevoyanmxitar7@gmail.com>
2026-08-16 12:28:09 +0400

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -12,7 +12,8 @@ document.addEventListener("DOMContentLoaded", function () {
         const phone = document.getElementById("phone").value.trim();
         const address = document.getElementById("address").value.trim();
 
-       if (!fullname  !phone  !address) {
+       if (!fullname || !phone || !address) {
+
             alert("Խնդրում ենք լրացնել բոլոր դաշտերը։");
             return;
         }
